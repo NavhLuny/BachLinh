11a5bd8c3c54d689d16831a676e588b4f009f8b6
NavhLuny <90888088+NavhLuny@users.noreply.github.com>
2021-11-07 14:05:42 +0700

    Add files via upload

---
+++ js/backtotop.js
@@ -0,0 +1,470 @@
+// JavaScript Document
+
+//page scroll
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+
+//scroll menu
+(function() {
+
+  'use strict';
+
+  var m = document.querySelector("main"),
+    h = document.querySelector("header"),
+    c = document.querySelector(".counter"),
+    counter = 0,
+    hHeight;
+
+  function setTopPadding() {
+    hHeight = h.offsetHeight;
+    m.style.paddingTop = hHeight + "px";
+  }
+
+  function onScroll() {
+
+    window.addEventListener("scroll", _.throttle(callbackFunc, 200));
+    /**
+     * Replace the line above with the following one 
+     * to see how many more times the "callbackFunc" is executed
+     */
+    // window.addEventListener("scroll", callbackFunc);
+
+    function callbackFunc() {
+      c.style.visibility = "visible";
+      c.innerHTML = counter++;
+      var y = window.pageYOffset;
+      if (y > 150) {
+        h.classList.add("scroll");
+      } else {
+        h.classList.remove("scroll");
+      }
+    }
+  }
+	window.onload = function() {
+  setTopPadding();
+  onScroll();
+};
+
+window.onresize = function() {
+  setTopPadding();
+};
+
+
+//BACK TO TOP
+// File#: _1_back-to-top
+// Usage: codyhouse.co/license
+(function() {
+  var backTop = document.getElementsByClassName('js-back-to-top')[0];
+  if( backTop ) {
+    var dataElement = backTop.getAttribute('data-element');
+    var scrollElement = dataElement ? document.querySelector(dataElement) : window;
+    var scrollDuration = parseInt(backTop.getAttribute('data-duration')) || 300, //scroll to top duration
+      scrollOffsetInit = parseInt(backTop.getAttribute('data-offset-in')) || parseInt(backTop.getAttribute('data-offset')) || 0, //show back-to-top if scrolling > scrollOffset
+      scrollOffsetOutInit = parseInt(backTop.getAttribute('data-offset-out')) || 0, 
+      scrollOffset = 0,
+      scrollOffsetOut = 0,
+      scrolling = false;
+
+    // check if target-in/target-out have been set
+    var targetIn = backTop.getAttribute('data-target-in') ? document.querySelector(backTop.getAttribute('data-target-in')) : false,
+      targetOut = backTop.getAttribute('data-target-out') ? document.querySelector(backTop.getAttribute('data-target-out')) : false;
+
+    updateOffsets();
+    
+    //detect click on back-to-top link
+    backTop.addEventListener('click', function(event) {
+      event.preventDefault();
+      if(!window.requestAnimationFrame) {
+        scrollElement.scrollTo(0, 0);
+      } else {
+        dataElement ? Util.scrollTo(0, scrollDuration, false, scrollElement) : Util.scrollTo(0, scrollDuration);
+      } 
+      //move the focus to the #top-element - don't break keyboard navigation
+      Util.moveFocus(document.getElementById(backTop.getAttribute('href').replace('#', '')));
+    });
+    
+    //listen to the window scroll and update back-to-top visibility
+    checkBackToTop();
+    if (scrollOffset > 0 || scrollOffsetOut > 0) {
+      scrollElement.addEventListener("scroll", function(event) {
+        if( !scrolling ) {
+          scrolling = true;
+          (!window.requestAnimationFrame) ? setTimeout(function(){checkBackToTop();}, 250) : window.requestAnimationFrame(checkBackToTop);
+        }
+      });
+    }
+
+    function checkBackToTop() {
+      updateOffsets();
+      var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+      if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+      var condition =  windowTop >= scrollOffset;
+      if(scrollOffsetOut > 0) {
+        condition = (windowTop >= scrollOffset) && (window.innerHeight + windowTop < scrollOffsetOut);
+      }
+      Util.toggleClass(backTop, 'back-to-top--is-visible', condition);
+      scrolling = false;
+    }
+
+    function updateOffsets() {
+      scrollOffset = getOffset(targetIn, scrollOffsetInit, true);
+      scrollOffsetOut = getOffset(targetOut, scrollOffsetOutInit);
+    }
+
+    function getOffset(target, startOffset, bool) {
+      var offset = 0;
+      if(target) {
+        var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+        if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+        var boundingClientRect = target.getBoundingClientRect();
+        offset = bool ? boundingClientRect.bottom : boundingClientRect.top;
+        offset = offset + windowTop;
+      }
+      if(startOffset && startOffset) {
+        offset = offset + parseInt(startOffset);
+      }
+      return offset;
+    }
+  }
+}());
+
+
+//SKILL
+(function($){
+        new WOW().init();
+    })(jQuery);
+/*
+
+Full-page view:
+
+https://codepen.io/GeorgePark/full/VXrwOP/
+
+*/
+
+// const posts = document.querySelectorAll('.gallery-item');
+
+// posts.forEach(post => {
+// 	post.addEventListener('click', () => {
+// 		//Get original image URL
+// 		const imgUrl = post.firstElementChild.src.split("?")[0];
+// 		//Open image in new tab
+// 		window.open(imgUrl, '_blank');
+// 	});
+// });
+
+
+//BLOG-NEW
+//Temporarily store data here
+const PostsData = [
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  },
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  }
+]
+
+
+// Start App
+
+class Main extends React.Component { 
+  constructor() {
+    super();
+    
+    this.state = {
+      posts: {}
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      posts: PostsData
+    });
+  }
+ 
+
+  render() {
+    return <div>
+      <header className="app-header"></header>
+      <Title />
+      <div className="app-card-list" id="app-card-list">
+        {
+          Object
+          .keys(this.state.posts)
+          .map(key => <Card key={key} index={key} details={this.state.posts[key]}/>)
+        }
+    </div>
+    </div>
+  }
+}
+
+
+class Title extends React.Component {
+  render() {
+    return <section className="app-title">
+      <div className="app-title-content">
+        <h1>Latest News</h1>
+        <p>Covering March & April 2015</p>
+        <a className="designer-link" href="https://dribbble.com/shots/1978243-Latest-News" target="_blank">Design from <i className="fa fa-dribbble"></i></a>
+      </div>
+    </section>
+  }
+}
+
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button button-primary">
+        <i className="fa fa-chevron-right"></i> Find out more
+      </button>
+    )
+  }
+}
+
+
+class CardHeader extends React.Component {
+  render() {
+    const { image, category } = this.props;
+    var style = { 
+        backgroundImage: 'url(' + image + ')',
+    };
+    return (
+      <header style={style} className="card-header">
+        <h4 className="card-header--title">{category}</h4>
+      </header>
+    )
+  }
+}
+
+
+class CardBody extends React.Component {
+  render() {
+    return (
+      <div className="card-body">
+        <p className="date">March 20 2015</p>
+        
+        <h2>{this.props.title}</h2>
+        
+        <p className="body-content">{this.props.text}</p>
+        
+        <Button />
+      </div>
+    )
+  }
+}
+
+
+class Card extends React.Component {
+  render() {
+    return (
+      <article className="card">
+        <CardHeader category={this.props.details.category} image={this.props.details.image}/>
+        <CardBody title={this.props.details.title} text={this.props.details.text}/>
+      </article>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+);
+
+
+// BOOTSRAPMIN
+//1
+// JavaScript Document
+//Temporarily store data here
+const PostsData = [
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  },
+  {
+    "category": "News",
+    "title": "CNN Acquire BEME",
+    "text": "CNN purchased Casey Neistat's Beme app for $25million.",
+    "image": "https://source.unsplash.com/user/erondu/600x400"
+  },
+  {
+    "category": "Travel",
+    "title": "Nomad Lifestyle",
+    "text": "Learn our tips and tricks on living a nomadic lifestyle",
+    "image": "https://source.unsplash.com/user/_vickyreyes/600x400"
+  },
+  {
+    "category": "Development",
+    "title": "React and the WP-API",
+    "text": "The first ever decoupled starter theme for React & the WP-API",
+    "image": "https://source.unsplash.com/user/ilyapavlov/600x400"
+  }
+]
+
+
+// Start App
+
+class Main extends React.Component { 
+  constructor() {
+    super();
+    
+    this.state = {
+      posts: {}
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      posts: PostsData
+    });
+  }
+ 
+
+  render() {
+    return <div>
+      <header className="app-header"></header>
+      <Title />
+      <div className="app-card-list" id="app-card-list">
+        {
+          Object
+          .keys(this.state.posts)
+          .map(key => <Card key={key} index={key} details={this.state.posts[key]}/>)
+        }
+    </div>
+    </div>
+  }
+}
+
+
+class Title extends React.Component {
+  render() {
+    return <section className="app-title">
+      <div className="app-title-content">
+        <h1>Latest News</h1>
+        <p>Covering March & April 2015</p>
+        <a className="designer-link" href="https://dribbble.com/shots/1978243-Latest-News" target="_blank">Design from <i className="fa fa-dribbble"></i></a>
+      </div>
+    </section>
+  }
+}
+
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button button-primary">
+        <i className="fa fa-chevron-right"></i> Find out more
+      </button>
+    )
+  }
+}
+
+
+class CardHeader extends React.Component {
+  render() {
+    const { image, category } = this.props;
+    var style = { 
+        backgroundImage: 'url(' + image + ')',
+    };
+    return (
+      <header style={style} className="card-header">
+        <h4 className="card-header--title">{category}</h4>
+      </header>
+    )
+  }
+}
+
+
+class CardBody extends React.Component {
+  render() {
+    return (
+      <div className="card-body">
+        <p className="date">March 20 2015</p>
+        
+        <h2>{this.props.title}</h2>
+        
+        <p className="body-content">{this.props.text}</p>
+        
+        <Button />
+      </div>
+    )
+  }
+}
+
+
+class Card extends React.Component {
+  render() {
+    return (
+      <article className="card">
+        <CardHeader category={this.props.details.category} image={this.props.details.image}/>
+        <CardBody title={this.props.details.title} text={this.props.details.text}/>
+      </article>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+);
+
+//2
+/* Demo purposes only */
+$(".hover").mouseleave(
+  function() {
+    $(this).removeClass("hover");
+  }
+);
